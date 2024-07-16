edd7f6ab0cf506db465b2ff26650d0e87518beca
lixuyang <lixy@shuzilm.cn>
2024-07-16 19:14:17 +0800

    order set

---
+++ internal/consts/const.go
@@ -0,0 +1,5 @@
+package consts
+
+type Empty struct{}
+
+var EMPTY = Empty{}
--- orderSet/order_set.go
+++ orderSet/order_set.go
@@ -1,6 +1,8 @@
-package set
+package orderSet
 
-import "github.com/xuyang-lee/ezSet/internal/consts"
+import (
+	"github.com/xuyang-lee/ezSet/internal/consts"
+)
 
 // Copyright (c) 2024, Lee.
 // All rights reserved.
@@ -39,53 +41,47 @@ import "github.com/xuyang-lee/ezSet/internal/consts"
 //  false
 // ============
 
-type Set[T comparable] struct {
+type OrderSet[T comparable] struct {
 	setMap map[T]consts.Empty
+	keys   []T
 }
 
-// NewSetOf create a new set,
+// NewOrderSetOf create a new set,
 // the type of the set will be the same as the type of t
-func NewSetOf[T comparable](t T) *Set[T] {
+func NewOrderSetOf[T comparable](t T) *OrderSet[T] {
 	return newSet[T]()
 }
 
-// NewSet create a new set,
+// NewOrderSet create a new set,
 // the type of the set is custom
-func NewSet[T comparable]() *Set[T] {
+func NewOrderSet[T comparable]() *OrderSet[T] {
 	return newSet[T]()
 }
 
-// NewSetWithSlice create a new set,
+// NewOrderSetWithSlice create a new set,
 // the type of the set will be the same as the type of l,
 // and new set will be init with l
-func NewSetWithSlice[T comparable](l []T) *Set[T] {
+func NewOrderSetWithSlice[T comparable](l []T) *OrderSet[T] {
 	s := newSet[T]()
+	s.keys = make([]T, 0, len(l))
 
 	for _, v := range l {
+		if _, ok := s.setMap[v]; ok {
+			continue
+		}
 		s.setMap[v] = consts.EMPTY
+		s.keys = append(s.keys, v)
 	}
-
 	return s
 }
 
-// Distinct delete duplicate data from the list, but do not guarantee the order of results
-func Distinct[T comparable](l []T) []T {
-	s := newSet[T]()
-
-	for _, v := range l {
-		s.setMap[v] = consts.EMPTY
-	}
-
-	return s.list()
-}
-
 // Len get the size of set
-func (s *Set[T]) Len() int {
+func (s *OrderSet[T]) Len() int {
 	return s.len()
 }
 
 // Add elements to the set
-func (s *Set[T]) Add(l ...T) {
+func (s *OrderSet[T]) Add(l ...T) {
 
 	for _, e := range l {
 		s.add(e)
@@ -94,141 +90,124 @@ func (s *Set[T]) Add(l ...T) {
 }
 
 // Remove elements from the set
-func (s *Set[T]) Remove(l ...T) {
+func (s *OrderSet[T]) Remove(l ...T) {
+	//粗略估计如果元素数量大于10000或者删除元素数量大于50，直接使用removeMany更高效
+	if s.len() > 10000 || len(l) > 50 {
+		s.removeMany(l)
+		return
+	}
+
 	for _, e := range l {
 		s.remove(e)
 	}
 	return
 }
 
+func (s *OrderSet[T]) RemoveMany(l ...T) {
+	s.removeMany(l)
+	return
+}
+
 // Clear get an empty set with origin type
-func (s *Set[T]) Clear() {
+func (s *OrderSet[T]) Clear() {
 	s.setMap = make(map[T]consts.Empty)
+	s.keys = make([]T, 0)
 	return
 }
 
 // Clone create a new set which has the same elements as s
-func (s *Set[T]) Clone() *Set[T] {
+func (s *OrderSet[T]) Clone() *OrderSet[T] {
 	return s.clone()
 }
 
 // List get all elements as a slice
-func (s *Set[T]) List() []T {
+func (s *OrderSet[T]) List() []T {
 	return s.list()
 }
 
 // Contains  whether e belongs to s
-func (s *Set[T]) Contains(e T) bool {
+func (s *OrderSet[T]) Contains(e T) bool {
 	return s.contains(e)
 }
 
 // IsSubSetOf  whether s is subset of d
-func (s *Set[T]) IsSubSetOf(d *Set[T]) bool {
+func (s *OrderSet[T]) IsSubSetOf(d *OrderSet[T]) bool {
 	return s.isSubSetOf(d)
 }
 
 // IsSuperSetOf  whether s is super set of d
-func (s *Set[T]) IsSuperSetOf(d *Set[T]) bool {
+func (s *OrderSet[T]) IsSuperSetOf(d *OrderSet[T]) bool {
 	return s.isSuperSetOf(d)
 }
 
 // IsEqual  whether s is the same as d
-func (s *Set[T]) IsEqual(d *Set[T]) bool {
+func (s *OrderSet[T]) IsEqual(d *OrderSet[T]) bool {
 	return s.isSubSetOf(d) && s.isSuperSetOf(d)
 }
 
-// Union 交集：return set = s * d
-func (s *Set[T]) Union(d *Set[T]) *Set[T] {
-	//创建交集
-	union := newSet[T]()
-	//遍历d中元素
-	for k := range d.setMap {
-		//若s中存在，则加入集合
-		if s.contains(k) {
-			union.add(k)
-		}
-	}
-
-	return union
-
-}
-
-// Intersect 并集：return set = s + d
-func (s *Set[T]) Intersect(d *Set[T]) *Set[T] {
-	intersect := s.clone()
-	for k := range d.setMap {
-		intersect.add(k)
-	}
-	return intersect
-}
-
-// Difference 差集：return set = s - d
-func (s *Set[T]) Difference(d *Set[T]) *Set[T] {
-	diff := newSet[T]()
-	for k := range s.setMap {
-		//在s中而不在d中
-		if !d.contains(k) {
-			diff.add(k)
-		}
-	}
-	//输出差集结果
-	return diff
-}
-
-// Complement 补集：return set = d - s
-func (s *Set[T]) Complement(d *Set[T]) *Set[T] {
-	comp := newSet[T]()
-	for k := range d.setMap {
-		//在d中而不在s中
-		if !s.contains(k) {
-			comp.add(k)
-		}
-	}
-	//输出差集结果
-	return comp
-}
-
-func newSet[T comparable]() *Set[T] {
-	s := new(Set[T])
+func newSet[T comparable]() *OrderSet[T] {
+	s := new(OrderSet[T])
 	s.setMap = make(map[T]consts.Empty)
 	return s
 }
 
-func (s *Set[T]) add(e T) {
+func (s *OrderSet[T]) add(e T) {
+	if s.contains(e) {
+		return
+	}
+	s.keys = append(s.keys, e)
 	s.setMap[e] = consts.EMPTY
+
 	return
 }
 
-func (s *Set[T]) remove(e T) {
+func (s *OrderSet[T]) remove(e T) {
+	if s.contains(e) {
+		delete(s.setMap, e)
+		s.find(e)
+		copy(s.keys[s.find(e):], s.keys[s.find(e)+1:])
+		s.keys = s.keys[:len(s.keys)-1]
+	}
+	return
+}
 
-	if _, ok := s.setMap[e]; ok {
+func (s *OrderSet[T]) removeMany(l []T) {
+	indicesToDelete := make(map[T]consts.Empty, len(l)) // 使用map为了快速查找
+	for _, e := range l {
+		indicesToDelete[e] = consts.EMPTY
+		//删除集合中的元素
 		delete(s.setMap, e)
 	}
 
-	return
+	//删除顺序切片中的元素
+	tmp := s.keys[:0] // 使用相同的底层数组创建新的切片
+	for _, key := range s.keys {
+		if _, found := indicesToDelete[key]; !found {
+			tmp = append(tmp, key)
+		}
+	}
+	s.keys = tmp
 }
 
-func (s *Set[T]) clone() *Set[T] {
+func (s *OrderSet[T]) clone() *OrderSet[T] {
 	c := newSet[T]()
-	for k := range s.setMap {
-		c.setMap[k] = consts.EMPTY
+	for _, key := range s.keys {
+		c.setMap[key] = consts.EMPTY
 	}
+	c.keys = make([]T, s.len())
+	copy(c.keys, s.keys)
 	return c
 }
 
-func (s *Set[T]) len() int {
+func (s *OrderSet[T]) len() int {
 	return len(s.setMap)
 }
 
-func (s *Set[T]) list() []T {
-	var l = make([]T, 0)
-	for k := range s.setMap {
-		l = append(l, k)
-	}
-	return l
+func (s *OrderSet[T]) list() []T {
+	return s.keys
 }
 
-func (s *Set[T]) isSubSetOf(d *Set[T]) bool {
+func (s *OrderSet[T]) isSubSetOf(d *OrderSet[T]) bool {
 	if s.len() > d.len() {
 		return false
 	}
@@ -241,7 +220,7 @@ func (s *Set[T]) isSubSetOf(d *Set[T]) bool {
 	return true
 }
 
-func (s *Set[T]) isSuperSetOf(d *Set[T]) bool {
+func (s *OrderSet[T]) isSuperSetOf(d *OrderSet[T]) bool {
 	if s.len() < d.len() {
 		return false
 	}
@@ -254,9 +233,16 @@ func (s *Set[T]) isSuperSetOf(d *Set[T]) bool {
 	return true
 }
 
-func (s *Set[T]) contains(e T) bool {
-	if _, ok := s.setMap[e]; ok {
-		return true
+func (s *OrderSet[T]) contains(e T) (ok bool) {
+	_, ok = s.setMap[e]
+	return
+}
+
+func (s *OrderSet[T]) find(e T) int {
+	for i, v := range s.keys {
+		if v == e {
+			return i
+		}
 	}
-	return false
+	return -1
 }
